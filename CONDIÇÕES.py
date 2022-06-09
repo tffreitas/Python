502be28f41ffd02701925f788a8c20cf2d59db83
Thiago Franco de Freitas <thiagofreitas2882@gmail.com>
2022-06-09 00:08:56 -0300

    add

---
+++ CONDIÇÕES.py
@@ -0,0 +1,22 @@
+# Condição
+
+'''if:
+
+else:'''
+
+#exemplos:
+
+tempo = int(input('Quantos anos seu carro tem de uso? '))
+if tempo<=3:
+    print('Carro novo')
+else:
+    print('Carro velho')
+print('--FIM--')
+
+nome = str(input('Qual seu nome? ')).capitalize()
+if nome == 'Thiago':
+    print('Que belo nome você tem!')
+print('Bom dia, {}!'.format(nome))
+
+
+
