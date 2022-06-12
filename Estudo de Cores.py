b2af77141137dc0a9e8763e1b5d0ae3e49f81e59
Thiago Franco de Freitas <thiagofreitas2882@gmail.com>
2022-06-11 21:43:00 -0300

    Create Estudo de Cores.py

---
+++ Estudo de Cores.py
@@ -0,0 +1,34 @@
+# Exemplo de codigo para cores no Terminal:
+# \033[0:33:44m
+
+#Onde após o [ é informado o estilo do texto, a cor do texto e a cor de fundo.
+
+#Para estilo os codigos usados são:
+# 0 - Nenhum
+# 1 - Negrito
+# 4 - Sublinhado
+# 7 - Invertido
+
+#Para cores do texto temos os codigos abaixo:
+
+# 30 - PRETO
+# 31 - VERMELHO
+# 32 - VERDE
+# 33 - AMARELO
+# 34 - AZUL
+# 35 - ROXO
+# 36 - AZUL CLARO
+# 37 - CINZA
+# 97 - BRANCO
+
+# Para as cores de fundo temos os codigos abaixo:
+
+# 40 - PRETO
+# 41 - VERMELHO
+# 42 - VERDE
+# 43 - AMARELO
+# 44 - AZUL
+# 45 - ROXO
+# 46 - AZUL CLARO
+# 47 - CINZA
+# 107 - BRANCO
