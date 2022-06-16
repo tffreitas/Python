27cec6718ddb88fe644043f5bead4791bb367e54
Thiago Franco de Freitas <thiagofreitas2882@gmail.com>
2022-06-15 23:18:16 -0300

    Update exe052.py

--- exe052.py
+++ exe052.py
@@ -1,9 +1,19 @@
 #Ler um numero inteiro e dizer se ele é ou não primo.
 
 num = int(input('Digite um numero: '))
-
+tot = 0
 for c in range(1, num+1):
-    print(c, end=' ')
-    '''if num / 1 == num and num / num == 1 and num % c == 0:
-        print('O numero {} foi divisivel,'.format(num))'''
+    if num % c == 0:
+        print('\033[1;34m', end=' ')
+        tot += 1
+    else:
+        print('\033[1;31m', end=' ')
+    print('{}'.format(c), end=' ')
+print('\n\33[m0 número {} foi divisível {} vezes'.format(num, tot))
+if tot == 2:
+    print('O número {} é PRIMO!'.format(num))
+else:
+    print('O número {} não é PRIMO!'.format(num))
+
+
 
