d8cfb3543b33134953894110dffb93120b906812
Thiago Franco de Freitas <thiagofreitas2882@gmail.com>
2022-06-30 22:42:41 -0300

    Update ESTRUTURADEREPETICAO FOR.py

--- ESTRUTURADEREPETICAO FOR.py
+++ ESTRUTURADEREPETICAO FOR.py
@@ -14,5 +14,11 @@ for c in range(0,10):
 for c in range(0,10):
     print(c+1)'''
 
+produtos = ['coca', 'pepsi', 'guarana', 'sprit', 'fanta']
+producao = ['15000', '12000', '13000', '5000', '250']
+
+tamanho = len(produtos)
+for i in range(tamanho):
+    print('{} unidades produzidas de {}'.format(producao[i], produtos[i]))
 
 
