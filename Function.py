93c39f9f7678333be16e72c56e444836fb8aa301
Thiago Franco de Freitas <thiagofreitas2882@gmail.com>
2022-07-02 21:49:17 -0300

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,16 @@
+#Function Pyton
+
+#Estrutura Basica:
+
+#def nome_funcao():
+#    return valor_final
+
+def cadastrar_produto():
+    produto = input('Digite o nome do produto para cadastro:')
+    produto = produto.lower()
+    produto = produto.strip()
+    print('Produto {} cadastrado com sucesso.'.format(produto))
+    return produto
+
+produto = cadastrar_produto()
+print(produto)
