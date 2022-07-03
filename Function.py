47f286800261369e0972bea7cc3807fa2a7a8698
Thiago Franco de Freitas <thiagofreitas2882@gmail.com>
2022-07-02 22:59:45 -0300

    Update Function.py

--- Function.py
+++ Function.py
@@ -5,7 +5,7 @@
 #def nome_funcao():
 #    return valor_final
 
-def cadastrar_produto():
+'''def cadastrar_produto():
     produto = input('Digite o nome do produto para cadastro:')
     produto = produto.lower()
     produto = produto.strip()
@@ -13,4 +13,10 @@ def cadastrar_produto():
     return produto
 
 produto = cadastrar_produto()
-print(produto)
+print(produto)'''
+
+'''texto = 'esse é o meu texto'''''
+frutas = ['maça', 'uva', 'banana', 'morango', 'goiaba']
+
+for i in frutas:
+    print(frutas)
