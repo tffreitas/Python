f2c03592a102bf9c870896c6f77278b45366c744
Thiago Franco de Freitas <thiagofreitas2882@gmail.com>
2022-07-03 22:08:58 -0300

    Update ESTRUTURA DE REPETIÇÃO WHILE.py

--- ESTRUTURA DE REPETIÇÃO WHILE.py
+++ ESTRUTURA DE REPETIÇÃO WHILE.py
@@ -11,8 +11,21 @@ while n != 0:
     n = int(input('Digite um valor: '))
 print('Fim')'''
 
-r = 'S'
+'''r = 'S'
 while r == 'S':
     n = int(input('Digite um numero: '))
     r = str(input('Quer continuar? [S/N] ')).upper()
-print('Fim')
+print('Fim')'''
+
+n = 1
+par = impar = 0
+while n != 0:
+    n = int(input('Digite um valor: '))
+    if n != 0:
+        if n % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+print('Você digitou {} numeros pares e {} numeros ímpares'.format(par, impar))
+
+
