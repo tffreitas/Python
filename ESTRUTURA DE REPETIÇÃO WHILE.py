fc694ccb315f2649fedc7e41d5ba08acd4976abc
Thiago Franco de Freitas <thiagofreitas2882@gmail.com>
2022-07-03 21:57:01 -0300

    Create ESTRUTURA DE REPETIÇÃO WHILE.py

---
+++ ESTRUTURA DE REPETIÇÃO WHILE.py
@@ -0,0 +1,18 @@
+#Exemplos:
+
+'''n = 1
+while n < 10:
+    print(n)
+    n += 1
+print('Fim')'''
+
+'''n = 1
+while n != 0:
+    n = int(input('Digite um valor: '))
+print('Fim')'''
+
+r = 'S'
+while r == 'S':
+    n = int(input('Digite um numero: '))
+    r = str(input('Quer continuar? [S/N] ')).upper()
+print('Fim')
